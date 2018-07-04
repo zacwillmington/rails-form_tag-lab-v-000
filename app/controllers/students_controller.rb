a093275602f3acb1f1181d6b6c851eecd8474cce
Zac Willmington <zac.willmington@gmail.com>
2018-07-04 19:33:06 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,7 +9,7 @@ class StudentsController < ApplicationController
   end
 
   def new 
-      @student = Student.create(params)
+      @student = Student.create(:first_name => params['student']['first_name'], :last_name => params['student']['last_name'])
   end
   
 end
