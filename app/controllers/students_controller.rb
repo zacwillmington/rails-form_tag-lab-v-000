e6d36206a7fb71ba4a8bd6e62aa0b014820e6de3
Zac Willmington <zac.willmington@gmail.com>
2018-07-04 19:33:30 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,6 +9,7 @@ class StudentsController < ApplicationController
   end
 
   def new 
+      binding.pry
       @student = Student.create(:first_name => params['student']['first_name'], :last_name => params['student']['last_name'])
   end
   
